1f1136f1eeed7ee52fede62038e9d8f8c6275649
sharmin <178066501+webwizsharmin@users.noreply.github.com>
2025-11-09 10:44:40 +0600

    selecting all essential element

---
+++ app.js
@@ -0,0 +1,12 @@
+// Selecting All Element
+const number = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(
+  ".operators:not(#erase):not(#backspace)"
+);
+const equal = document.querySelector("#equal");
+const backspace = document.querySelector("#backspace");
+const clear = document.querySelector("#erase");
+const sqrtBtn = document.querySelector("#sqrt");
+const percentBtn = document.querySelector("#percenBtn");
+const realTimeInput = document.querySelector("#realTimeInput");
+const result = document.querySelector("#result");
